db0aa5fd0892a46fec5586cc317e52d4a91af3c4
Simon Hudon <simon.hudon@gmail.com>
2017-08-13 13:56:52 -0400

    added anamorphisms and catamorphisms on fix-point data structures

--- f_algebra.lean
+++ f_algebra.lean
@@ -2,20 +2,22 @@
 universe u
 
 inductive fix (f : Type u → Type u) : Type (u+1)
-  | fix (β : Type u) : f β → (β → fix) → fix
+  | fix (n : ℕ) : f (ulift $ fin n) → (fin n → fix) → fix
 
 structure identity (x : Type u) : Type u :=
   (get : x)
 
+open ulift
+
 #check fix identity
 
 def of_nat : ℕ → fix option
- | 0 := (fix.fix empty none (empty.rec _))
- | (nat.succ n) := fix.fix unit (some ()) (λ _, of_nat n)
+ | 0 := (fix.fix 0 none fin.elim0)
+ | (nat.succ n) := fix.fix 1 (some $ ulift.up 0) (λ _, of_nat n)
 
 def to_nat : fix option → ℕ
  | (fix.fix β none f) := 0
- | (fix.fix β (some x) f) := nat.succ $ to_nat (f x)
+ | (fix.fix β (some x) f) := nat.succ $ to_nat (f $ down x)
 
 example {n : ℕ} : to_nat (of_nat n) = n :=
 begin
@@ -39,12 +41,12 @@ inductive link (α β : Type u) : Type u
 #check fix (link ℕ)
 
 def of_list {α} : list α → fix (link α)
- | [] := (fix.fix empty link.nil (empty.rec _))
- | (x :: xs) := fix.fix unit (link.mk x ()) (λ _, of_list xs)
+ | [] := (fix.fix 0 link.nil fin.elim0)
+ | (x :: xs) := fix.fix 1 (link.mk x $ up 0) (λ _, of_list xs)
 
 def to_list {α} : fix (link α) → list α
  | (fix.fix β link.nil f) := []
- | (fix.fix β (link.mk x xs) f) := x :: to_list (f xs)
+ | (fix.fix β (link.mk x xs) f) := x :: to_list (f $ down xs)
 
 example {α} {xs : list α} : to_list (of_list xs) = xs :=
 begin
@@ -71,15 +73,28 @@ inductive tree (α : Type u) : Type u
 
 #check fix (node ℕ)
 
+def list_array {α} (xs : list α) (i : fin xs.length) : α :=
+list.nth_le xs i.val i.is_lt
+
+@[simp]
+lemma list_array_zero {α} (x : α) (xs : list α)
+: list_array (x :: xs) (0 : fin (nat.succ _)) = x := rfl
+
+@[simp]
+lemma list_array_one {α} (x₀ x₁ : α) (xs : list α)
+: list_array (x₀ :: x₁ :: xs) (1 : fin (nat.succ _)) = x₁ := rfl
+
 def of_tree {α} : tree α → fix (node α)
- | tree.leaf := (fix.fix empty node.nil (empty.rec _))
- | (tree.node t₀ x t₁) := fix.fix bool
-                              (node.mk ff x tt)
-                              (λ b, bool.rec_on b (of_tree t₀) (of_tree t₁))
+ | tree.leaf := (fix.fix 0 node.nil fin.elim0)
+ | (tree.node t₀ x t₁) := fix.fix 2
+                              (node.mk (up 0) x (up 1))
+                              (list_array [of_tree t₀, of_tree t₁])
 
 def to_tree {α} : fix (node α) → tree α
  | (fix.fix β node.nil f) := tree.leaf
- | (fix.fix β (node.mk t₀ x t₁) f) := tree.node (to_tree $ f t₀) x (to_tree $ f t₁)
+ | (fix.fix β (node.mk t₀ x t₁) f) :=
+tree.node (to_tree $ f $ down t₀) x
+          (to_tree $ f $ down t₁)
 
 example {α} {xs : tree α} : to_tree (of_tree xs) = xs :=
 begin
@@ -117,7 +132,7 @@ indices_aux n (le_refl n)
 
 def of_ntree {α} : ntree α → fix (n_node α)
  | (ntree.node x ts) :=
-fix.fix (fin ts.length) ⟨ (x,indices _) ⟩
+fix.fix ts.length ⟨ (x,up <$> indices _) ⟩
         (λ i,
            have ntree.sizeof α (list.nth_le ts (i.val) i.is_lt) < 1 + list.sizeof ts,
              begin
@@ -135,7 +150,7 @@ section
 variable {α : Type u}
 
 def to_ntree : fix (n_node α) → ntree α
- | (fix.fix β ⟨ (x,xs) ⟩ f) := ntree.node x (list.map (λ i, to_ntree (f i)) xs)
+ | (fix.fix β ⟨ (x,xs) ⟩ f) := ntree.node x (list.map (λ i, to_ntree (f $ down i)) xs)
 
 end
 
@@ -145,7 +160,7 @@ begin
   unfold of_ntree to_ntree,
   apply congr_arg,
   induction a_1,
-  { simp [indices,indices_aux,list.map] },
+  { simp [indices,indices_aux,list.map], refl },
   { admit }
 end
 
@@ -154,4 +169,30 @@ begin
   induction f with β x f,
   cases x ;
   admit
+end
+
+def cata {α} {F : Type u → Type u} [has_map F] (f : F α → α) : fix F → α
+ | (fix.fix β x g) := f $ (λ i, cata (g $ down i)) <$> x
+
+class foldable (f : Type u → Type u) extends functor f :=
+  (size : ∀ {α}, f α → ℕ)
+  (fold : ∀ {α} (x : f α), fin (size x) → α)
+  (idx : ∀ {α} (x : f α), f (ulift $ fin (size x)))
+  (correct_fold : ∀ {α} (x : f α), map (λ i, (fold x) (ulift.down i)) (idx x) = x)
+
+export foldable (size fold idx correct_fold)
+
+section
+
+parameters {F : Type u → Type u} [foldable F]
+parameters {α : Type u}
+parameters (r : α → α → Prop) (wf : well_founded r)
+parameter (f : ∀ x : α, F ({ y : α // r y x }))
+
+def ana : α → fix F :=
+wf.fix $ λ x ana,
+  fix.fix _ (idx $ f x) $ λ j,
+    let y := @fold F _ _ (f x) j in
+    ana y y.property
+
 end
